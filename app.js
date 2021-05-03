e1d6a289a7d1b9fc2234ec22bd759fc9630e6701
Orlando Brea <orlando.brea@gmail.com>
2021-05-03 19:19:30 -0300

    feature: agrego telefonos a la api

--- app.js
+++ app.js
@@ -39,4 +39,38 @@ app.delete('/api/personas/:id', (req, res) => {
     listado = listado.filter(item => item.id != req.params.id);
     res.json({});
 });
+
+
+let listadoTelefonos = [];
+
+app.get('/api/telefonos', (req, res) => {
+    res.json(listadoTelefonos);
+});
+
+app.get('/api/telefonos/:id', (req, res) => {
+    const itemFound = listadoTelefonos.find(item => item.id == req.params.id);
+    if (itemFound) res.json(itemFound);
+    else res.status(404).send();
+});
+
+app.post('/api/telefonos', (req, res) => {
+    const newItem = {
+        ...req.body,
+        id: uniqid(),
+    };
+    listadoTelefonos = [...listadoTelefonos, newItem];
+    res.json(newItem);
+});
+
+app.put('/api/telefonos/:id', (req, res) => {
+    const toUpdate = {...req.body, id: req.params.id};
+    listadoTelefonos = listadoTelefonos.map(item => (item.id == req.params.id ? toUpdate : item));
+    res.json(toUpdate);
+});
+
+app.delete('/api/telefonos/:id', (req, res) => {
+    listadoTelefonos = listadoTelefonos.filter(item => item.id != req.params.id);
+    res.json({});
+});
+
 app.listen(PORT, () => console.log(`App corriendo en el puerto ${PORT}`));
